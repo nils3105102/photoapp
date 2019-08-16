764309364481f66dc7a18c7259275852e7a33229
nils3105102 <daniil.pron4enkov@gmail.com>
2019-08-16 12:30:52 +0300

    dependences

--- resources/views/inc/topbar.blade.php
+++ resources/views/inc/topbar.blade.php
@@ -3,8 +3,8 @@
     <div class="top-bar-left">
       <ul class="menu">
         <li class="menu-text">PhotoShow</li>
-        <li><a href="/">Home</a></li>
-        <li><a href="/albums/create">Create Album</a></li>
+        <li><a href="{{url('/')}}">Home</a></li>
+        <li><a href="{{url('/albums/create')}}">Create Album</a></li>
       </ul>
     </div>
   </div>
